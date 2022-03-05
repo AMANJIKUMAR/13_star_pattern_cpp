b8ccb1cb16c594fcd2c36fbbe9ab380cf8a13c94
Aman kumar <72993406+AMANJIKUMAR@users.noreply.github.com>
2022-03-05 22:18:46 +0530

    Add files via upload

---
+++ 13_star_patten.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int row,col;
+    int n;
+    cin>>n;
+     for(row = 1; row<=n ; row=row+1){
+         for (col = 1; col<=row ; col=col+1){
+             cout<<'*';
+
+         }
+         cout<<endl;
+     }
+     cout<<endl;
+     return 0;
+}
+
+
+
+//      o/p
+//      *
+//      **
+//      ***
+//      ****
+//      *****
+//      ******
